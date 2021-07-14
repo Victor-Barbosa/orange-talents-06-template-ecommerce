bb16da253780481ffc958ae1a10e77cd5847aac8
Victor-Barbosa <victorbarbosadev@gmail.com>
2021-07-14 20:18:34 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/victor/mercadolivre/OrangeTalents06TemplateEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.victor.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents06TemplateEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTalents06TemplateEcommerceApplication.class, args);
+    }
+
+}
